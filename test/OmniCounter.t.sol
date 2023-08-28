33d512507c2327a5a2d6963dcaa9a5c0b64554c7
Genesis3800 <priyankgupta0308@gmail.com>
2023-08-28 08:04:24 +0530

    testing completed

--- test/OmniCounter.t.sol
+++ test/OmniCounter.t.sol
@@ -1,13 +1,18 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.19;
 
+//Imports to use Foundry's testing and logging suite
 import {Test, console2} from "forge-std/Test.sol";
 
+//Imports to use the LayerZero contracts
 import {LZEndpointMock} from "@layerzero-contracts/mocks/LZEndpointMock.sol";
-
 import {OmniCounter_Sepolia} from "../src/OmniCounter_Sepolia.sol";
 import {OmniCounter_Mumbai} from "../src/OmniCounter_Mumbai.sol";
 
+/**
+ * @title OmniCounterTest
+ * @dev This contract is used to test the functionality of the OmniCounter contracts.
+ */
 contract OmniCounterTest is Test {
 
     LZEndpointMock public lzEndpointMock;
@@ -15,8 +20,11 @@ contract OmniCounterTest is Test {
     OmniCounter_Sepolia public omniCounter_Sepolia;
     OmniCounter_Mumbai public omniCounter_Mumbai;
 
-    uint16 public ChainId = 1221;
+    uint16 public constant ChainId = 1221;
 
+    /**
+     * @dev Sets up the initial state for the test.
+     */
     function setUp() public {
 
         vm.deal(address(0x1), 100 ether);
@@ -25,22 +33,21 @@ contract OmniCounterTest is Test {
 
         vm.prank(address(0x1));
         lzEndpointMock = new LZEndpointMock(ChainId);
-        address mockAddress = address(lzEndpointMock);
 
         vm.prank(address(0x2));
-        omniCounter_Sepolia = new OmniCounter_Sepolia(mockAddress);
+        omniCounter_Sepolia = new OmniCounter_Sepolia(address(lzEndpointMock));
 
         vm.prank(address(0x3));
-        omniCounter_Mumbai = new OmniCounter_Mumbai(mockAddress);
+        omniCounter_Mumbai = new OmniCounter_Mumbai(address(lzEndpointMock));
 
-        vm.deal(mockAddress, 100 ether);
+        vm.deal(address(lzEndpointMock), 100 ether);
         vm.deal(address(omniCounter_Sepolia), 100 ether);
         vm.deal(address(omniCounter_Mumbai), 100 ether);
 
 
         vm.startPrank(address(0x1));
-        lzEndpointMock.setDestLzEndpoint(address(omniCounter_Sepolia), mockAddress);
-        lzEndpointMock.setDestLzEndpoint(address(omniCounter_Mumbai), mockAddress);
+        lzEndpointMock.setDestLzEndpoint(address(omniCounter_Sepolia), address(lzEndpointMock));
+        lzEndpointMock.setDestLzEndpoint(address(omniCounter_Mumbai), address(lzEndpointMock));
         vm.stopPrank();
         
         bytes memory omniCounter_Mumbai_Address = abi.encodePacked(uint160(address(omniCounter_Mumbai)));
@@ -55,16 +62,30 @@ contract OmniCounterTest is Test {
 
     }
 
+    /**
+     * @dev Tests the counter increment from Sepolia to Mumbai.
+    */
     function test_SepoliaToMumbai() public {
 
         uint counter_initial = omniCounter_Sepolia.counter();
-        console2.log("counter_initial", counter_initial);
 
         vm.deal(address(0x10), 100 ether);
         vm.prank(address(0x10));
         omniCounter_Sepolia.incrementCounter{value: 1 ether}(ChainId);
         assertEq(omniCounter_Mumbai.counter(), counter_initial+1);
-        //console2.log("counter_final", omniCounter_Mumbai.counter());
+    }
+
+    /**
+     * @dev Tests the counter increment from Mumbai to Sepolia.
+    */
+    function test_MumbaiToSepolia() public {
+
+        uint counter_initial = omniCounter_Mumbai.counter();
+
+        vm.deal(address(0x10), 100 ether);
+        vm.prank(address(0x10));
+        omniCounter_Mumbai.incrementCounter{value: 1 ether}(ChainId);
+        assertEq(omniCounter_Sepolia.counter(), counter_initial+1);
     }
  
 }
